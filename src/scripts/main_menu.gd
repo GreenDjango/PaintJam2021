074cd5f47bbccdb7aa66427a77abc5fc427cfeb0
Théo <52924057+GreenDjango@users.noreply.github.com>
2021-05-02 16:55:21 +0200

    ADD: bounce BUTTON

--- src/scripts/main_menu.gd
+++ src/scripts/main_menu.gd
@@ -3,8 +3,15 @@ extends Control
 func _ready():
 	pass
 
-func _input(event):
-	if event is InputEventKey:
-		if event.pressed:
-			Globals.goto_scene("game")
+func _on_Start_mouse_entered():
+	$Start.modulate = Color("dddddd")
 
+func _on_Start_mouse_exited():
+	$Start.modulate = Color.white
+
+func _on_Start_button_down():
+	$AnimationPlayer.play("Start")
+
+func _on_Start_button_up():
+	$AnimationPlayer.play_backwards("Start")
+	Globals.goto_scene("game")
