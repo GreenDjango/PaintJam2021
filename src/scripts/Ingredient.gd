db7c355ba9e119504abbdb771a849b7396ea14ed
Killian DROULEZ <killian.droulez@outlook.fr>
2021-05-02 10:27:29 +0200

    Add drag & drop behavior

--- src/scripts/Ingredient.gd
+++ src/scripts/Ingredient.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
 var selected = false
+var isMoving = true
 
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(),25 * delta)
+	if isMoving:
+		translate(Vector2(1.3,0))
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	print("event")
 	if Input.is_action_just_pressed("left_click"):
-		print("click")
 		selected = true
---
+++ src/scripts/level.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+onready var spawn_ingredient = $SpawnIngredient
+onready var ingredient = preload("res://src/nodes/Ingredient.tscn")
+
+var ingredientsInstancied = []
+var ingredientDragged = null
+var isIngredientInDespositArea = false
+
+func _ready():
+	instanceIngredient()
+
+func _physics_process(delta):
+	if isIngredientInDespositArea and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		ingredientDragged.queue_free()
+
+func instanceIngredient():
+	var new_ingredient = ingredient.instance()
+	new_ingredient.position = spawn_ingredient.position
+	add_child(new_ingredient)
+	ingredientsInstancied.append(new_ingredient)
+
+func _on_DepositArea_area_entered(area):
+	var ingredient = area.get_node("../../") # Getting the Ingredient node
+	if ingredient.is_in_group("Item") :
+		isIngredientInDespositArea = true
+		ingredientDragged = ingredient
+
+func _on_DepositArea_area_exited(area):
+	var ingredient = area.get_node("../../") # Getting the Ingredient node
+	if ingredient.is_in_group("Item") :
+		isIngredientInDespositArea = false
+		ingredientDragged = null
+
+func _on_DispawnIngredient_area_entered(area):
+	var ingredient = area.get_node("../../") # Getting the Ingredient node
+	if ingredient.is_in_group("Item") :
+		ingredient.queue_free()
+
